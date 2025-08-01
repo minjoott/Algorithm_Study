3d8365fb9ca993bf07434cfc56e48de8eb1058af
minjoott <minjoott.dev@gmail.com>
2025-08-01 17:52:03 +0900

    [250801] 1 solved 1. [Programmers] 187780. 연속된 부분 수열의 합 2. [LeetCode] 49. Group Anagrams

---
+++ src/rereview/leetcode/GroupAnagrams.java
@@ -0,0 +1,21 @@
+package rereview.leetcode;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+
+            String key = String.valueOf(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
